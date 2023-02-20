5a54234555a081449f2370422f84ef682ac09254
zilluiro <zilluiro@gmail.com>
2023-02-20 22:27:37 +0200

    add markup for the 'book-list' component

--- src/app/components/edit-book/edit-book.component.ts
+++ src/app/components/edit-book/edit-book.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-edit-book',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class EditBookComponent {
 
+  constructor() {}
 }
