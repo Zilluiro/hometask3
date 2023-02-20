edb5d11d86658ec8ff5b0babb00bf3efa59b7df9
zilluiro <zilluiro@gmail.com>
2023-02-21 00:06:26 +0200

    implement 'book-list-item' and 'book-list-item' components

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BookService} from "../../services/book.service";
+import {Observable} from "rxjs";
+import {BookModel} from "../../models/book-model";
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit{
+  books$: Observable<BookModel[]> | undefined;
 
+  constructor(private bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.getAllBooks();
+  }
+
+  getRecommendedBooks(): void{
+    this.books$ = this.bookService.getRecommendedBooks();
+  }
+
+  getAllBooks(): void{
+    this.books$ = this.bookService.getAllBooks();
+  }
 }
