4b5c0acd59b5706059cacb2bbb8aeaf186f2a602
zilluiro <zilluiro@gmail.com>
2023-02-21 00:41:04 +0200

    add modal window

--- src/app/components/view-book/view-book.component.ts
+++ src/app/components/view-book/view-book.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {BookModel} from "../../models/book-model";
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-view-book',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-book.component.css']
 })
 export class ViewBookComponent {
+  @Input() book: BookModel | undefined;
+  constructor(private activeModal: NgbActiveModal) {
+  }
 
+  close() {
+    this.activeModal.close();
+  }
 }
